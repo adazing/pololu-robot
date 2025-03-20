8fcedb975f45f1f7f78068332ea7e1c1e59293ef
adazing <109737747+adazing@users.noreply.github.com>
2025-03-20 12:00:52 -0400

    Update pololu_robot_tour.ino

--- pololu_robot_tour.ino
+++ pololu_robot_tour.ino
@@ -13,12 +13,12 @@ ButtonA buttonA;
 // R = right
 // L = left
 // T = stop/terminate
-char paths[] = "FFFBBBFFFBBBT";
+char paths[] = "SFFFT";
 bool timeAdj = true;
 bool encoderAdj = true;
 // char paths[] = "S";
 int index = 0;
-float targetTime = 5.0;
+float targetTime = 10.0;
 float baseSpeed = 100.0;
 
 //angle pid
@@ -26,14 +26,16 @@ float kp = 1;
 float error = 0.0;
 float leftSpeed = baseSpeed;
 float rightSpeed = baseSpeed;
-float minSpeed = 20.0;
+float minSpeed = 40.0;
 float maxSpeed = 255.0;
 
 //adjust time PID
 int maxCounts = 0;
-float kpTime = 0.0005;
+float kpTime = 0.005;
 long errorTime = 0.0;
 long startTime = 0.0;
+long startActionTime = 0.0;
+bool startedAction = true;
 
 // distances
 const float wheelDiameter = 3.2; // cm
@@ -52,6 +54,13 @@ int16_t actionDist = 0;
 int16_t countsLeftOffset = 0;
 int16_t countsRightOffset = 0;
 volatile int totalCount = 0;
+volatile int targetActionCount = 0;
+float targetActionTime = 0;
+float timeAccelProp = 0.2; // proportion of action spent accelerating and descelerating
+
+// acceleration math
+float kAccel = 0.001;
+float VM = 0.0;
 
 // int16_t accelDiff = 50; // speed diff between slow and medium and medium and high
 
@@ -73,17 +82,41 @@ void setup() {
   if (!encoderAdj){
     kp = 0.0;
   }
-  delay(1000);
+  delay(5000);
   startTime = millis();
+  Serial.println("hi");
 }
 
 void adjustSpeeds() {
   error = countsLeftDist - countsRightDist;
-  // errorTime = (totalCount + actionDist)/(float)maxCounts - ()
-  errorTime = (totalCount + actionDist)/(float)maxCounts*targetTime*1000 - (millis()-startTime);
+  float pred_ticks = 0.0;
+  float time_since_action_started = ((float)millis()-(float)startActionTime)/1000.0;
+  Serial.println(kAccel*VM);
+  Serial.println(time_since_action_started);
+  Serial.println(1/kAccel);
+  // Serial.println(startActionTime);
+  if (time_since_action_started<kAccel*VM){
+    Serial.println(1);
+    pred_ticks = (1/kAccel)*pow(time_since_action_started, 2)/2;
+  }else if (time_since_action_started<targetActionTime-kAccel*VM){
+    Serial.println(2);
+    pred_ticks = VM*kAccel*VM/2+VM*(time_since_action_started-VM*kAccel);
+  }else if (time_since_action_started<targetActionTime){
+    Serial.println(3);
+    pred_ticks = VM*kAccel*VM/2+(targetActionTime - 2*VM*kAccel)*VM + 1/kAccel*pow(time_since_action_started-(targetActionTime - kAccel*VM), 2)/2 + VM*(time_since_action_started - (targetActionTime - kAccel*VM));
+  }
+    Serial.print(leftSpeed);
+    Serial.print(" | ");
+    Serial.println(rightSpeed);
+  // Serial.print(pred_ticks);
+  // Serial.print(" ");
+  // Serial.println(actionDist);
+  errorTime = actionDist - pred_ticks;
+  // errorTime = (totalCount + actionDist)/(float)maxCounts*targetTime*1000 - (millis()-startTime);
   float adj_time = constrain(1 - kpTime*errorTime, 0.1, 2);
-  leftSpeed = constrain(adj_time*(baseSpeed - kp*error), minSpeed, maxSpeed);
-  rightSpeed = constrain(adj_time*(baseSpeed + kp*error), minSpeed, maxSpeed);
+  // Serial.println(adj_time);
+  leftSpeed = constrain(adj_time*(baseSpeed) - kp*error, minSpeed, maxSpeed);
+  rightSpeed = constrain(adj_time*(baseSpeed) + kp*error, minSpeed, maxSpeed);
 }
 
 void loop() {
@@ -94,13 +127,35 @@ void loop() {
     countsLeftDist = countsLeft;
     countsRightDist = countsRight;
     actionDist = max(countsLeftDist, countsRightDist);
-    if (actionDist > countsPerForward/2){
+    targetActionCount = countsPerForward/2;
+    targetActionTime = targetTime * (float)targetActionCount/(float)maxCounts;
 
-      totalCount += (int16_t)countsPerForward/2;
-      countsLeftOffset -= (int16_t)countsPerForward/2;
-      countsRightOffset -= (int16_t)countsPerForward/2;
+    if (startedAction){
+      if (pow(targetActionTime, 2)<4*kAccel*targetActionCount){
+        VM = targetActionTime/(2*kAccel);
+        Serial.println("cry1");
+      }else{
+        VM = (targetActionTime - sqrt(pow(targetActionTime, 2) - 4*kAccel*targetActionCount))/(2*kAccel);
+      }
+      if (2*kAccel*VM > targetActionTime){ // fallback --> cry and cry and cry omfg what the heck is happening
+        Serial.println("cry2");
+        VM = targetActionTime/(2*kAccel);
+      }
+      // Serial.println(kAccel);
+      // Serial.println(targetActionTime);
+      // Serial.println(targetActionCount);
+      // Serial.println(VM);
+      // Serial.println(kAccel*VM);
+      startActionTime = millis();
+      startedAction = false;
+    }
+    if (actionDist > targetActionCount){
+
+      totalCount += (int16_t)targetActionCount;
+      countsLeftOffset -= (int16_t)targetActionCount;
+      countsRightOffset -= (int16_t)targetActionCount;
       index++;
-
+      startedAction = true;
     }
     adjustSpeeds();
     motors.setSpeeds(leftSpeed, rightSpeed);
@@ -108,11 +163,33 @@ void loop() {
     countsLeftDist = countsLeft;
     countsRightDist = countsRight;
     actionDist = max(countsLeftDist, countsRightDist);
-    if (max(countsLeftDist, countsRightDist)>countsPerForward){
-      totalCount += (int16_t)countsPerForward;
-      countsLeftOffset -= (int16_t)countsPerForward;
-      countsRightOffset -= (int16_t)countsPerForward;
+    targetActionCount = countsPerForward;
+    targetActionTime = targetTime * (float)targetActionCount/(float)maxCounts;
+    if (startedAction){
+      if (pow(targetActionTime, 2)<4*kAccel*targetActionCount){
+        VM = targetActionTime/(2*kAccel);
+      }else{
+        VM = (targetActionTime - sqrt(pow(targetActionTime, 2) - 4*kAccel*targetActionCount))/(2*kAccel);
+      }
+      if (2*kAccel*VM > targetActionTime){ // fallback --> cry and cry and cry omfg what the heck is happening
+        Serial.println("cry");
+        VM = targetActionTime/(2*kAccel);
+      }
+      // Serial.println(kAccel);
+      // Serial.println(targetActionTime);
+      // Serial.println(targetActionCount);
+      // Serial.println(VM);
+      // Serial.println(kAccel*VM);
+      startActionTime = millis();
+      startedAction = false;
+    }
+
+    if (actionDist>targetActionCount){
+      totalCount += (int16_t)targetActionCount;
+      countsLeftOffset -= (int16_t)targetActionCount;
+      countsRightOffset -= (int16_t)targetActionCount;
       index++;
+      startedAction = true;
     }
     adjustSpeeds();
     motors.setSpeeds(leftSpeed, rightSpeed);
@@ -120,11 +197,32 @@ void loop() {
     countsLeftDist = -countsLeft;
     countsRightDist = -countsRight;
     actionDist = max(countsLeftDist, countsRightDist);
-    if (max(countsLeftDist, countsRightDist)>countsPerForward){
-      totalCount += (int16_t)countsPerForward;
-      countsLeftOffset += (int16_t)countsPerForward;
-      countsRightOffset += (int16_t)countsPerForward;
+    targetActionCount = countsPerForward;
+    targetActionTime = targetTime * (float)targetActionCount/(float)maxCounts;
+    if (startedAction){
+      if (pow(targetActionTime, 2)<4*kAccel*targetActionCount){
+        VM = targetActionTime/(2*kAccel);
+      }else{
+        VM = (targetActionTime - sqrt(pow(targetActionTime, 2) - 4*kAccel*targetActionCount))/(2*kAccel);
+      }
+      if (2*kAccel*VM > targetActionTime){ // fallback --> cry and cry and cry omfg what the heck is happening
+        Serial.println("cry");
+        VM = targetActionTime/(2*kAccel);
+      }
+      // Serial.println(kAccel);
+      // Serial.println(targetActionTime);
+      // Serial.println(targetActionCount);
+      // Serial.println(VM);
+      // Serial.println(kAccel*VM);
+      startActionTime = millis();
+      startedAction = false;
+    }
+    if (actionDist>targetActionCount){
+      totalCount += (int16_t)targetActionCount;
+      countsLeftOffset += (int16_t)targetActionCount;
+      countsRightOffset += (int16_t)targetActionCount;
       index++;
+      startedAction = true;
     }
     adjustSpeeds();
     motors.setSpeeds(-leftSpeed, -rightSpeed);
@@ -132,12 +230,32 @@ void loop() {
     countsLeftDist = countsLeft;
     countsRightDist = -countsRight;
     actionDist = max(countsLeftDist, countsRightDist);
-    if (max(countsLeftDist, countsRightDist)>countsPerTurn){
-      totalCount += (int16_t)countsPerTurn;
-      countsLeftOffset -= (int16_t)countsPerTurn;
-      countsRightOffset += (int16_t)countsPerTurn;
+    targetActionCount = countsPerTurn;
+    targetActionTime = targetTime * (float)targetActionCount/(float)maxCounts;
+    if (startedAction){
+      if (pow(targetActionTime, 2)<4*kAccel*targetActionCount){
+        VM = targetActionTime/(2*kAccel);
+      }else{
+        VM = (targetActionTime - sqrt(pow(targetActionTime, 2) - 4*kAccel*targetActionCount))/(2*kAccel);
+      }
+      if (2*kAccel*VM > targetActionTime){ // fallback --> cry and cry and cry omfg what the heck is happening
+        Serial.println("cry");
+        VM = targetActionTime/(2*kAccel);
+      }
+      // Serial.println(kAccel);
+      // Serial.println(targetActionTime);
+      // Serial.println(targetActionCount);
+      // Serial.println(VM);
+      // Serial.println(kAccel*VM);
+      startActionTime = millis();
+      startedAction = false;
+    }
+    if (actionDist>targetActionCount){
+      totalCount += (int16_t)targetActionCount;
+      countsLeftOffset -= (int16_t)targetActionCount;
+      countsRightOffset += (int16_t)targetActionCount;
       index++;
-
+      startedAction = true;
     }
     adjustSpeeds();
     motors.setSpeeds(leftSpeed, -rightSpeed);
@@ -145,12 +263,33 @@ void loop() {
     countsLeftDist = -countsLeft;
     countsRightDist = countsRight;
     actionDist = max(countsLeftDist, countsRightDist);
-    if (max(countsLeftDist, countsRightDist)>countsPerTurn){
+    targetActionCount = countsPerTurn;
+    targetActionTime = targetTime * (float)targetActionCount/(float)maxCounts;
+    if (startedAction){
+      if (pow(targetActionTime, 2)<4*kAccel*targetActionCount){
+        VM = targetActionTime/(2*kAccel);
+      }else{
+        VM = (targetActionTime - sqrt(pow(targetActionTime, 2) - 4*kAccel*targetActionCount))/(2*kAccel);
+      }
+      if (2*kAccel*VM > targetActionTime){ // fallback --> cry and cry and cry omfg what the heck is happening
+        Serial.println("cry");
+        VM = targetActionTime/(2*kAccel);
+      }
+      // Serial.println(kAccel);
+      // Serial.println(targetActionTime);
+      // Serial.println(targetActionCount);
+      // Serial.println(VM);
+      // Serial.println(kAccel*VM);
+      startActionTime = millis();
+      startedAction = false;
+    }
+    if (actionDist>targetActionCount){
 
-      totalCount += (int16_t)countsPerTurn;
-      countsLeftOffset += (int16_t)countsPerTurn;
-      countsRightOffset -= (int16_t)countsPerTurn;
+      totalCount += (int16_t)targetActionCount;
+      countsLeftOffset += (int16_t)targetActionCount;
+      countsRightOffset -= (int16_t)targetActionCount;
       index++;
+      startedAction = true;
 
     }
     adjustSpeeds();
